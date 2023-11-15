ef14cf8598859b7d1dad3c92ebda0cf8bc3941b5
Tyuiu.ChalkovaEM@outlook.com <tch4cat@yandex.ru>
2023-11-15 21:01:44 +0500

    Add project files.

---
+++ Tyuiu.ChalkovaE.M.Sprint5.Task0.V15/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChalkovaE.M.Sprint5.Task0.V15.Lib;
+
+namespace Tyuiu.ChalkovaE.M.Sprint5.Task0.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Чалкова Е. М. | ИИПб-23-2";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                             *");
+            Console.WriteLine("* Задание #0                                                                   *");
+            Console.WriteLine("* Вариант #15                                                                  *");
+            Console.WriteLine("* Выполнила Чалкова Е. М. | ИИПб-23-2                                          *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в       *");
+            Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до трёх   *");
+            Console.WriteLine("* знаков после запятой.                                                        *");
+            Console.WriteLine("*             x                                                                *");
+            Console.WriteLine("*    y =  --------                                                             *");
+            Console.WriteLine("*          x^3 + 2                                                             *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+            int x = 3;
+            Console.WriteLine("Значение переменной X = " + x);
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
