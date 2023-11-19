ffd137738231344db00e681674a83c1af4336be9
Tyuiu.ChalkovaEM@outlook.com <tch4cat@yandex.ru>
2023-11-19 15:30:30 +0500

    Task2

---
+++ Tyuiu.ChalkovaE.M.Sprint5.Task2.V22/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChalkovaE.M.Sprint5.Task2.V22.Lib;
+
+namespace Tyuiu.ChalkovaE.M.Sprint5.Task2.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Чалкова Е. М. | ИИПб-23-2";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                             *");
+            Console.WriteLine("* Задание #0                                                                   *");
+            Console.WriteLine("* Вариант #15                                                                  *");
+            Console.WriteLine("* Выполнила Чалкова Е. М. | ИИПб-23-2                                          *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов,                         *");
+            Console.WriteLine("* заполненный значениями с клавиатуры. Заменить положительные элементы массива *");
+            Console.WriteLine("* на 1, отрицательные на 0. Результат сохранить в файл                         *");
+            Console.WriteLine("* OutPutFileTask2.csv и вывести на консоль.                                    *");
+            Console.WriteLine("* -8; -1; -9                                                                   *");
+            Console.WriteLine("*  8; 5; 5                                                                     *");
+            Console.WriteLine("*  9; 6; -2                                                                    *");
+            
+            
+            int[,] mtrx = new int[3, 3] {{ -8, -1, -9}, {8, 5, 5}, {9, 6, -2}};
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
