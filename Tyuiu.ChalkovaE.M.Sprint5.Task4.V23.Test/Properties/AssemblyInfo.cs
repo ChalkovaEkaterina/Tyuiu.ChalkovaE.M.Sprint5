0446f95995baeb6685468250ef6aa1417b6a401f
Tyuiu.ChalkovaEM@outlook.com <tch4cat@yandex.ru>
2023-11-25 17:19:37 +0500

    Task4

---
+++ Tyuiu.ChalkovaE.M.Sprint5.Task4.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChalkovaE.M.Sprint5.Task4.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChalkovaE.M.Sprint5.Task4.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0e235a5-2a9b-4105-afd1-67f596ef42f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
